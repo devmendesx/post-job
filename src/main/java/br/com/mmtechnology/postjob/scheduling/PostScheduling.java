1b2623796572d5f5722b5a8cc05bf5bccf1bdd01
Matheus Mendes <matheushenriquesad@gmail.com>
2024-04-12 23:41:37 -0300

    PTJ - Cron time.

--- src/main/java/br/com/mmtechnology/postjob/scheduling/PostScheduling.java
+++ src/main/java/br/com/mmtechnology/postjob/scheduling/PostScheduling.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
