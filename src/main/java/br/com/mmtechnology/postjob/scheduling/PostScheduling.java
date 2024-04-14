d094dd9b27b96d6713860405801e97c99d128cc2
Matheus Mendes <matheushenriquesad@gmail.com>
2024-04-14 00:53:46 -0300

    PTJ - Cron time.

--- src/main/java/br/com/mmtechnology/postjob/scheduling/PostScheduling.java
+++ src/main/java/br/com/mmtechnology/postjob/scheduling/PostScheduling.java
@@ -26,7 +26,7 @@ public class PostScheduling {
   @Value("${gowhats.token}")
   private String token;
 
-  @Scheduled(cron = "5 10 * * * *")
+  @Scheduled(cron = "0 5 * * * *")
   public void sendSignal() {
     try {
       try {
